c48b5adce079ed9412416d7f039c8a553b961e0d
porterbot <schauderd@janelia.hhmi.org>
2018-01-31 11:57:19 -0500

    added userTags mouselight preference to deal with ownership issues with hide/show

--- src/main/java/org/janelia/model/domain/tiledMicroscope/TmNeuronTagMap.java
+++ src/main/java/org/janelia/model/domain/tiledMicroscope/TmNeuronTagMap.java
@@ -1,5 +1,6 @@
 package org.janelia.model.domain.tiledMicroscope;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,11 +17,13 @@ import com.google.common.collect.SetMultimap;
 public class TmNeuronTagMap {
 
     public static final ImmutableSet<String> PREDEFINED_TAGS = ImmutableSet.of(
-            "auto"
+            "auto", "hidden"
     );
 
     // here's our many-to-many maps; keep them synchronized, which
     //  means methods have to be synchronized
+
+    private Map<Long,Set<String>> userTags = new HashMap<>();
     private SetMultimap<TmNeuronMetadata, String> tags = HashMultimap.create();
     private SetMultimap<String, TmNeuronMetadata> neurons = HashMultimap.create();
     private Map<String,Map<String,Object>> tagGroupMappings = new HashMap<>();
@@ -55,6 +58,32 @@ public class TmNeuronTagMap {
     public synchronized void addTag(String tag, TmNeuronMetadata neuron) {
         tags.put(neuron, tag);
         neurons.put(tag, neuron);
+    }
+
+    public synchronized void addUserTag(String tag, TmNeuronMetadata neuron) {
+        Set userNeuronSet = userTags.get(neuron.getId());
+        if (userNeuronSet==null) {
+            userNeuronSet = new HashSet<String>();
+            userTags.put(neuron.getId(),userNeuronSet);
+        }
+        userNeuronSet.add(tag);
+    }
+
+    public synchronized void removeUserTag(String tag, TmNeuronMetadata neuron) {
+        Set userNeuronTags = userTags.get(neuron.getId());
+        if (userNeuronTags!=null) {
+            userNeuronTags.remove(tag);
+            userTags.put(neuron.getId(),userNeuronTags);
+        }
+
+    }
+
+    public synchronized Map<Long, Set<String>> getUserTags() {
+        return userTags;
+    }
+
+    public synchronized void setUserTags(Map<Long, Set<String>> map) {
+        userTags = map;
     }
 
     public synchronized void setTagGroupMapping(String tag, Map<String,Object> meta) {
