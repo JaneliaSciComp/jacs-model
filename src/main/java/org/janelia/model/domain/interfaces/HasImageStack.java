4705415967c74428e88d9c78932871d111188b10
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-02-02 17:48:23 -0500

    added convenience methods

--- src/main/java/org/janelia/model/domain/interfaces/HasImageStack.java
+++ src/main/java/org/janelia/model/domain/interfaces/HasImageStack.java
@@ -1,11 +1,22 @@
 package org.janelia.model.domain.interfaces;
 
+import org.janelia.model.access.domain.DomainUtils;
+import org.janelia.model.domain.enums.FileType;
+
 /**
  * Methods for exposing an underlying image stack.
  * 
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
-public interface HasImageStack {
+public interface HasImageStack extends HasFiles {
+
+    default String getLosslessStack() {
+        return DomainUtils.getFilepath(this, FileType.LosslessStack);
+    }
+
+    default String getVisuallyLosslessStack() {
+        return DomainUtils.getFilepath(this, FileType.VisuallyLosslessStack);
+    }
 
     public String getAnatomicalArea();
 
