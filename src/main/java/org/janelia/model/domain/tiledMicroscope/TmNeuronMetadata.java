315b54168e78537398f587fd08bc50e2c9b3448b
porterbot <schauderd@janelia.hhmi.org>
2018-02-07 16:43:41 -0500

    added ownerkey to copy method

--- src/main/java/org/janelia/model/domain/tiledMicroscope/TmNeuronMetadata.java
+++ src/main/java/org/janelia/model/domain/tiledMicroscope/TmNeuronMetadata.java
@@ -55,6 +55,7 @@ public class TmNeuronMetadata extends AbstractDomainObject {
         copy.setWorkspaceRef(neuron.getWorkspaceRef());
         copy.setVisible(neuron.isVisible());
         copy.setColorHex(neuron.getColorHex());
+        copy.setOwnerKey(neuron.getOwnerKey());
         copy.setTags(new HashSet<String>(neuron.getTags()));
         return copy;
     }
