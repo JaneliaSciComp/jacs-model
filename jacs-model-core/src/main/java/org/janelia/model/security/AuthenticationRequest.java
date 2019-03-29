afa404288201dbd56936b8cbfc0e59fa9480cee4
porterbot <schauderd@janelia.hhmi.org>
2019-03-29 08:54:31 -0400

    Added endpoints and methods for Group/User Management

---
+++ jacs-model-core/src/main/java/org/janelia/model/security/AuthenticationRequest.java
@@ -0,0 +1,26 @@
+package org.janelia.model.security;
+
+/**
+ * Created by schauderd on 3/29/19.
+ */
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
