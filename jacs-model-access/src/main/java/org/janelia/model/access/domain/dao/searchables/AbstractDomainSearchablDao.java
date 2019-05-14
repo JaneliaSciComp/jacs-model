cd561dd78dd86a139baec9cf247f9969439e08e1
Cristian Goina <goinac@hhmi.org>
2019-05-14 14:58:02 -0400

    ontology dao that indexes the updated ontologies

---
+++ jacs-model-access/src/main/java/org/janelia/model/access/domain/dao/searchables/AbstractDomainSearchablDao.java
@@ -0,0 +1,115 @@
+package org.janelia.model.access.domain.dao.searchables;
+
+import org.janelia.model.access.cdi.AsyncIndex;
+import org.janelia.model.access.domain.dao.DaoUpdateResult;
+import org.janelia.model.access.domain.dao.DomainObjectDao;
+import org.janelia.model.access.domain.dao.EntityFieldValueHandler;
+import org.janelia.model.access.domain.search.DomainObjectIndexer;
+import org.janelia.model.domain.DomainObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Abstract Mongo DAO.
+ *
+ * @param <T> type of the element
+ */
+public abstract class AbstractDomainSearchablDao<T extends DomainObject>
+        implements DomainObjectDao<T> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractDomainSearchablDao.class);
+
+    final DomainObjectDao<T> domainObjectDao;
+    final DomainObjectIndexer domainObjectIndexer;
+
+    AbstractDomainSearchablDao(DomainObjectDao<T> domainObjectDao,
+                               DomainObjectIndexer domainObjectIndexer) {
+        this.domainObjectDao = domainObjectDao;
+        this.domainObjectIndexer = domainObjectIndexer;
+    }
+
+    @Override
+    public T findById(Long id) {
+        return domainObjectDao.findById(id);
+    }
+
+    @Override
+    public List<T> findByIds(Collection<Long> ids) {
+        return domainObjectDao.findByIds(ids);
+    }
+
+    @Override
+    public List<T> findAll(long offset, int length) {
+        return domainObjectDao.findAll(offset, length);
+    }
+
+    @Override
+    public T findEntityByIdAccessibleBySubjectKey(Long id, String subjectKey) {
+        return domainObjectDao.findEntityByIdAccessibleBySubjectKey(id, subjectKey);
+    }
+
+    @Override
+    public List<T> findEntitiesByIdsAccessibleBySubjectKey(List<Long> ids, String subjectKey) {
+        return domainObjectDao.findEntitiesByIdsAccessibleBySubjectKey(ids, subjectKey);
+    }
+
+    @Override
+    public List<T> findOwnedEntitiesBySubjectKey(String subjectKey, long offset, int length) {
+        return domainObjectDao.findOwnedEntitiesBySubjectKey(subjectKey, offset, length);
+    }
+
+    @Override
+    public List<T> findEntitiesReadableBySubjectKey(@Nullable String subjectKey, long offset, int length) {
+        return domainObjectDao.findEntitiesReadableBySubjectKey(subjectKey, offset, length);
+    }
+
+    @Override
+    public long deleteByIdAndSubjectKey(Long id, String subjectKey) {
+        long n = domainObjectDao.deleteByIdAndSubjectKey(id, subjectKey);
+        if (n > 0) {
+            domainObjectIndexer.removeDocument(id);
+        }
+        return n;
+    }
+
+    @Override
+    public T saveBySubjectKey(T entity, String subjectKey) {
+        return null;
+    }
+
+    @Override
+    public void save(T entity) {
+
+    }
+
+    @Override
+    public void saveAll(Collection<T> entities) {
+
+    }
+
+    @Override
+    public void delete(T entity) {
+        domainObjectDao.delete(entity);
+        domainObjectIndexer.removeDocument(entity.getId());
+    }
+
+    @Override
+    public DaoUpdateResult update(Long entityId, Map<String, EntityFieldValueHandler<?>> fieldsToUpdate) {
+        DaoUpdateResult updateResult = domainObjectDao.update(entityId, fieldsToUpdate);
+        if (updateResult.getEntitiesAffected() > 0) {
+            try {
+                T entityRef = getEntityType().newInstance();
+                domainObjectIndexer.indexDocument(entityRef);
+            } catch (Exception e) {
+                LOG.error("Error creating a reference to {} of type {} for indexing", entityId, getEntityType(), e);
+            }
+        }
+        return updateResult;
+    }
+
+}
---
+++ jacs-model-access/src/main/java/org/janelia/model/access/domain/dao/searchables/OntologySearchableDao.java
@@ -0,0 +1,50 @@
+package org.janelia.model.access.domain.dao.searchables;
+
+import org.janelia.model.access.cdi.AsyncIndex;
+import org.janelia.model.access.domain.dao.OntologyDao;
+import org.janelia.model.access.domain.search.DomainObjectIndexer;
+import org.janelia.model.domain.ontology.Ontology;
+import org.janelia.model.domain.ontology.OntologyTerm;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * {@link Ontology} Mongo DAO.
+ */
+@AsyncIndex
+public class OntologySearchableDao extends AbstractDomainSearchablDao<Ontology> implements OntologyDao {
+
+    private final OntologyDao ontologyDao;
+
+    @Inject
+    OntologySearchableDao(OntologyDao ontologyDao,
+                          @AsyncIndex DomainObjectIndexer objectIndexer) {
+        super(ontologyDao, objectIndexer);
+        this.ontologyDao = ontologyDao;
+    }
+
+    @Override
+    public List<Ontology> getOntologiesAccessibleBySubjectGroups(String subjectKey, long offset, int length) {
+        return ontologyDao.getOntologiesAccessibleBySubjectGroups(subjectKey, offset, length);
+    }
+
+    @Override
+    public Ontology addTerms(String subjectKey, Long ontologyId, Long parentTermId, List<OntologyTerm> terms, Integer pos) {
+        Ontology updatedOntology = ontologyDao.addTerms(subjectKey, ontologyId, parentTermId, terms, pos);
+        domainObjectIndexer.indexDocument(updatedOntology);
+        return updatedOntology;
+    }
+
+    @Override
+    public Ontology reorderTerms(String subjectKey, Long ontologyId, Long parentTermId, int[] order) {
+        return ontologyDao.reorderTerms(subjectKey, ontologyId, parentTermId, order);
+    }
+
+    @Override
+    public Ontology removeTerm(String subjectKey, Long ontologyId, Long parentTermId, Long termId) {
+        Ontology updatedOntology = ontologyDao.removeTerm(subjectKey, ontologyId, parentTermId, termId);
+        domainObjectIndexer.indexDocument(updatedOntology);
+        return updatedOntology;
+    }
+}
