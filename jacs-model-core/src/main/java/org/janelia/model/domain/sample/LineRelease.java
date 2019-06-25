abdad611b081069790a042de03dadd9436526094
Konrad Rokicki <rokickik@janelia.hhmi.org>
2019-06-25 09:52:20 -0400

    turn LineRelease into a node with children

--- jacs-model-core/src/main/java/org/janelia/model/domain/sample/LineRelease.java
+++ jacs-model-core/src/main/java/org/janelia/model/domain/sample/LineRelease.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.janelia.model.domain.AbstractDomainObject;
+import org.janelia.model.domain.Reference;
 import org.janelia.model.domain.support.MongoMapped;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.janelia.model.domain.support.SearchTraversal;
+import org.janelia.model.domain.workspace.Node;
 
 /**
  * A release definition which controls how Samples are released and published to the external website. 
@@ -15,16 +18,43 @@ import com.fasterxml.jackson.annotation.JsonFormat;
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
 @MongoMapped(collectionName="release",label="Line Release")
-public class LineRelease extends AbstractDomainObject {
+public class LineRelease extends AbstractDomainObject implements Node {
 
+    @Deprecated
 	private boolean autoRelease;
+
+    @Deprecated
     @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssX")
     private Date releaseDate;
+
+    @Deprecated
     private Integer lagTimeMonths;
-    private boolean sageSync;    
+
+    @Deprecated
+    private boolean sageSync;
+
+    @Deprecated
     private List<String> dataSets = new ArrayList<>();
+
+    @Deprecated
     private List<String> annotators = new ArrayList<>();
+
+    @Deprecated
     private List<String> subscribers = new ArrayList<>();
+
+    @SearchTraversal({})
+    private List<Reference> children = new ArrayList<>();
+
+    @Override
+    public List<Reference> getChildren() {
+        return children;
+    }
+
+    @Override
+    public void setChildren(List<Reference> children) {
+        if (children==null) throw new IllegalArgumentException("Property cannot be null");
+        this.children = children;
+    }
 
 	public boolean isAutoRelease() {
 		return autoRelease;
