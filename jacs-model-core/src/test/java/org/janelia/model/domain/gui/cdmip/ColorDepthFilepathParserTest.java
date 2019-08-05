222ec221f19012fbb5f6c7f9020ead3a894a688f
Cristian Goina <goinac@janelia.hhmi.org>
2019-08-05 14:50:57 -0400

    fixed the test for windows

--- jacs-model-core/src/test/java/org/janelia/model/domain/gui/cdmip/ColorDepthFilepathParserTest.java
+++ jacs-model-core/src/test/java/org/janelia/model/domain/gui/cdmip/ColorDepthFilepathParserTest.java
@@ -3,6 +3,8 @@ package org.janelia.model.domain.gui.cdmip;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * Tests for the ColorDepthFilepathParser.
  * 
@@ -16,7 +18,7 @@ public class ColorDepthFilepathParserTest {
         String filepath = "/nrs/jacs/jacsData/filestore/system/ColorDepthMIPs/JFRC2013_63x/flylight_gen1_mcfo_case_1/GMR_SS00313-20170324_26_C5-40x-Brain-JFRC2013_63x-2391787101235445858-CH1_CDM.png";
 
         ColorDepthFilepathParser parser = ColorDepthFilepathParser.parse(filepath);
-        Assert.assertEquals(filepath, parser.getFile().getAbsolutePath());
+        Assert.assertEquals(new File(filepath).getAbsolutePath(), parser.getFile().getAbsolutePath());
         Assert.assertEquals("Sample#2391787101235445858", parser.getSampleRef().toString());
         Assert.assertEquals("JFRC2013_63x", parser.getAlignmentSpace());
         Assert.assertEquals("GMR_SS00313-20170324_26_C5", parser.getSampleName());
@@ -31,7 +33,7 @@ public class ColorDepthFilepathParserTest {
         String filepath = "/groups/jacs/jacsDev/devstore/system/ColorDepthMIPs/JRC2018_VNC_Unisex/flylight_splitgal4_drivers/GMR_SS00810-20140421_31_D1-20x-VNC-JRC2018_VNC_Unisex-2002548901436981346-CH1_CDM.png";
 
         ColorDepthFilepathParser parser = ColorDepthFilepathParser.parse(filepath);
-        Assert.assertEquals(filepath, parser.getFile().getAbsolutePath());
+        Assert.assertEquals(new File(filepath).getAbsolutePath(), parser.getFile().getAbsolutePath());
         Assert.assertEquals("Sample#2002548901436981346", parser.getSampleRef().toString());
         Assert.assertEquals("JRC2018_VNC_Unisex", parser.getAlignmentSpace());
         Assert.assertEquals("GMR_SS00810-20140421_31_D1", parser.getSampleName());
@@ -46,7 +48,7 @@ public class ColorDepthFilepathParserTest {
         String filepath = "/groups/jacs/jacsDev/devstore/system/ColorDepthMIPs/JRC2018_VNC_Unisex/flylight_splitgal4_drivers/extra/nesting/GMR_SS00810-20140421_31_D1-20x-VNC-JRC2018_VNC_Unisex-2002548901436981346-CH1_CDM.png";
 
         ColorDepthFilepathParser parser = ColorDepthFilepathParser.parse(filepath);
-        Assert.assertEquals(filepath, parser.getFile().getAbsolutePath());
+        Assert.assertEquals(new File(filepath).getAbsolutePath(), parser.getFile().getAbsolutePath());
         Assert.assertEquals("Sample#2002548901436981346", parser.getSampleRef().toString());
         Assert.assertEquals("JRC2018_VNC_Unisex", parser.getAlignmentSpace());
         Assert.assertEquals("GMR_SS00810-20140421_31_D1", parser.getSampleName());
