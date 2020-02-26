e9bdd3070095d450650c47aba6e787c356286039
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-02-26 16:50:36 -0500

    added new TimebasedIdentifierGenerator from JACSv2

---
+++ jacs-model-core/src/test/java/org/janelia/model/util/TimebasedIdentifierGeneratorTest.java
@@ -0,0 +1,25 @@
+package org.janelia.model.util;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+
+public class TimebasedIdentifierGeneratorTest {
+    private TimebasedIdentifierGenerator idGenerator;
+
+    @Before
+    public void setUp() {
+        idGenerator = new TimebasedIdentifierGenerator(0);
+    }
+
+    @Test
+    public void generateLargeListOfIds() {
+        List<Number> idList = idGenerator.generateIdList(16384);
+        assertThat(ImmutableSet.copyOf(idList), hasSize(idList.size()));
+    }
+}
