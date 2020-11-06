4827b197ad7e7ac04928ba7ffe4b4df4c501237c
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-11-06 15:02:51 -0500

    removed unused pipelines (synchronized with JACSv1)

--- jacs-model-core/src/main/java/org/janelia/model/domain/sample/PipelineProcess.java
+++ jacs-model-core/src/main/java/org/janelia/model/domain/sample/PipelineProcess.java
@@ -2,6 +2,14 @@ package org.janelia.model.domain.sample;
 
 import org.janelia.model.domain.enums.NamedEnum;
 
+/**
+ * Describes all the possible image processing pipelines used by the Workstation's JACSv1 backend.
+ *
+ * Currently this file must be manually synchronized with the PipelineProcess in the private JACSv1 legacy code base
+ * at https://github.com/JaneliaSciComp/janelia-workstation.
+ *
+ * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
+ */
 public enum PipelineProcess implements NamedEnum {
 
     // The enum must match the process file name after PipelineConfig_
@@ -15,22 +23,8 @@ public enum PipelineProcess implements NamedEnum {
     FlyLightProjectionWithChannelSpec("FlyLight Projection With Channel Spec Pipeline"),
 
     FlyLightMCFO40x2018("FlyLight MCFO 40x 2018 Alignment Pipeline"),
-    FlyLightPairedSample2018("FlyLight Paired Sample 2018 Alignment Pipeline"),
-    FlyLightPairedSample2018MCFO("FlyLight Paired MCFO 2018 Alignment Pipeline"),
-    FlyLightPairedSample2018Polarity("FlyLight Paired Polarity 2018 Alignment Pipeline"),
-    FlyLightPairedSample2018MCFOFull("FlyLight Paired MCFO 2018 (Full Scale 63x) Alignment Pipeline"),
-    FlyLightPairedSample2018PolarityFull("FlyLight Paired Polarity 2018 (Full Scale 63x) Alignment Pipeline"),
     FlyLightPolarityCase3("FlyLight Polarity Case 3 Pipeline"),
     FlyLightPolarityCase5("FlyLight Polarity Case 5 Pipeline"),
-
-    FlyLightCentralBrain("Legacy FlyLight Central Brain Pipeline"),
-    FlyLightOpticLobe("Legacy FlyLight Optic Lobe Pipeline"),
-    FlyLightWholeBrain("Legacy FlyLight Whole Brain Pipeline"),
-    FlyLightWholeBrain64x("Legacy FlyLight Whole Brain 63x Pipeline"),
-    FlyLightPairedSample("Legacy FlyLight Paired Sample"),
-    FlyLightPairedSamplePolarity("Legacy FlyLight Paired Sample Polarity"),
-    FlyLightPairedSampleMCFO("Legacy FlyLight Paired Sample MCFO"),
-    FlyLightMCFO40x("Legacy FlyLight MCFO 40X Pipeline"),
 
     PTR10ProjectionOnly("PTR 10 Projection Only Pipeline"),
     PTR30CMTKAlignment20x63x("PTR 30 CMTK Alignment (20x/63x) Pipeline"),
@@ -38,8 +32,6 @@ public enum PipelineProcess implements NamedEnum {
     PTR30Tango20x63x("PTR 30 Tango (20x/63x) Pipeline"),
     PTR30Tango40x("PTR 30 Tango (40x) Pipeline"),
 
-    NernaLeftOpticLobe("Nern Left Optic Lobe 63x Pipeline"),
-    NernaRightOpticLobe("Nern Right Optic Lobe 63x Pipeline"),
     NernaMCFOCase1("Nern MCFO Case 1 Pipeline"),
     NernaMCFOCase1WithoutGrouper("Nern MCFO Case 1 (Without Grouper) Pipeline"),
     NernaMCFOCase1WithGrouper("Nern MCFO Case 1 (With Grouper) Pipeline"),
@@ -47,32 +39,35 @@ public enum PipelineProcess implements NamedEnum {
     NernaPolarityCase3("Nern Polarity Case 3 Pipeline"),
     NernaPolarityCase3WithoutGrouper("Nern Polarity Case 3 (Without Grouper) Pipeline"),
     NernaPolarityCase3WithGrouper("Nern Polarity Case 3 (With Grouper) Pipeline"),
+    NernaLeftOpticLobe("Legacy Nern Left Optic Lobe 63x Pipeline"),
+    NernaRightOpticLobe("Legacy Nern Right Optic Lobe 63x Pipeline"),
+    FlyLightWholeBrain("Legacy Nern Whole Brain Pipeline"),
 
     WolfftMCFOCase1("Wolff Central Brain MCFO Case 1 Pipeline"),
     WolfftMCFOCase1Unaligned("Wolff MCFO Case 1 Unaligned Pipeline"),
 
-    YoshiMB63xFlpout1024pxINT("Aso MB Flp-out 63x 1024px INTensity Pipeline"),
-    YoshiMB63xFlpout512pxINT("Aso MB Flp-out 63x 512px INTensity Pipeline"),
-    YoshiMB63xLexAGal41024pxINT("Aso MB LexA-GAL4 63x 1024px INTensity Pipeline"),
-    YoshiMB63xLexAGal4512pxINT("Aso MB LexA-GAL4 63x 512px INTensity Pipeline"),
+    YoshiMB63xFlpout1024pxINT("Legacy Aso MB Flp-out 63x 1024px Pipeline"),
+    YoshiMB63xFlpout512pxINT("Legacy Aso MB Flp-out 63x 512px Pipeline"),
+    YoshiMB63xLexAGal41024pxINT("Legacy Aso MB LexA-GAL4 63x 1024px Pipeline"),
+    YoshiMB63xLexAGal4512pxINT("Legacy Aso MB LexA-GAL4 63x 512px Pipeline"),
     YoshiMBPolarityCase1("Aso MB Polarity Case 1 Pipeline"),
     YoshiMBPolarityCase2("Aso MB Polarity Case 2 Pipeline"),
     YoshiMBPolarityCase3("Aso MB Polarity Case 3 Pipeline"),
     YoshiMBPolarityCase4("Aso MB Polarity Case 4 Pipeline"),
     YoshiMBSplitMCFOCase1("Aso MB Split MCFO Case 1 Pipeline"),
 
-    LeetCentralBrain63x("Lee Central Brain 63x Pipeline"),
-    LeetWholeBrain40x("Lee Whole Brain 40x Pipeline"),
-    LeetWholeBrain40x512pxINT("Lee Whole Brain 40x 512px INTensity Pipeline"),
-    LeetWholeBrain40xImproved("Lee Whole Brain 40x Improved Pipeline"),
     LeetUnaligned("Lee Unaligned Pipeline"),
     LeetUnalignedNoSeparation("Lee Unaligned/No Separation Pipeline"),
+    LeetCentralBrain63x("Legacy Lee Central Brain 63x Pipeline"),
+    LeetWholeBrain40x("Legacy Lee Whole Brain 40x Pipeline"),
+    LeetWholeBrain40x512pxINT("Legacy Lee Whole Brain 40x 512px INTensity Pipeline"),
+    LeetWholeBrain40xImproved("Legacy Lee Whole Brain 40x Improved Pipeline"),
 
     TrumanLarval40x("Truman Larval 40x Pipeline");
 
     private String name;
 
-    private PipelineProcess(String name) {
+    PipelineProcess(String name) {
         this.name = name;
     }
 
