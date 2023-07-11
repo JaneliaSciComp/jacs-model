990e8b2d811f51ca31c2c82588fc255e3f3b9125
porterbot <porterrobot@gmail.com>
2023-07-11 10:16:10 -0400

    fixed mongo serialization

--- jacs-model-core/src/main/java/org/janelia/model/domain/tiledMicroscope/BoundingBox3d.java
+++ jacs-model-core/src/main/java/org/janelia/model/domain/tiledMicroscope/BoundingBox3d.java
@@ -1,5 +1,7 @@
 package org.janelia.model.domain.tiledMicroscope;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class BoundingBox3d
 {
     protected double[] min = new double[3];
@@ -18,14 +20,17 @@ public class BoundingBox3d
         this.max = max;
     }
 
+    @JsonIgnore
     public double[] getCenter() {
         return new double[]{max[0]+min[0]/2.0, max[1]+min[1]/2.0, max[2]+min[2]/2.0};
     }
 
+    @JsonIgnore
     public double getDepth() {
         return max[2] - min[2];
     }
 
+    @JsonIgnore
     public double getHeight() {
         return max[1] - min[2];
     }
@@ -34,18 +39,25 @@ public class BoundingBox3d
         return max;
     }
 
+    @JsonIgnore
     public double getMaxX() {return max[0];}
+    @JsonIgnore
     public double getMaxY() {return max[1];}
+    @JsonIgnore
     public double getMaxZ() {return max[2];}
 
     public double[] getMin() {
         return min;
     }
 
+    @JsonIgnore
     public double getMinX() {return min[0];}
+    @JsonIgnore
     public double getMinY() {return min[1];}
+    @JsonIgnore
     public double getMinZ() {return min[2];}
 
+    @JsonIgnore
     public double getWidth() {
         return max[0] - min[0];
     }
