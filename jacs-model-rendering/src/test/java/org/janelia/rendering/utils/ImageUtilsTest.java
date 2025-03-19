92106aaaeaddd65fc4aa6ef444a35dc0e98ea040
Cristian Goina <goinac@hhmi.org>
2025-03-19 11:39:46 -0400

    minor refactoring to make the code a little bit more clear

--- jacs-model-rendering/src/test/java/org/janelia/rendering/utils/ImageUtilsTest.java
+++ jacs-model-rendering/src/test/java/org/janelia/rendering/utils/ImageUtilsTest.java
@@ -1,9 +1,6 @@
 package org.janelia.rendering.utils;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -55,40 +52,30 @@ public class ImageUtilsTest {
     @Test
     public void loadTiffPixelBytes() throws IOException {
         TestUtils.prepareTestDataFiles(Paths.get(TEST_DATADIR), testDirectory, "default.0.tif");
-        File testFile = testDirectory.resolve("default.0.tif").toFile();
+        Path testFilePath = testDirectory.resolve("default.0.tif");
         RenderedImageInfo imageInfo;
-        try (InputStream tiffStream = Files.newInputStream(testFile.toPath())) {
+        try (InputStream tiffStream = Files.newInputStream(testFilePath)) {
             imageInfo = ImageUtils.loadImageInfoFromTiffStream(tiffStream);
+            assertNotNull(imageInfo);
         }
-        try (InputStream tiffStream = Files.newInputStream(testFile.toPath())) {
+        try (InputStream tiffStream = Files.newInputStream(testFilePath)) {
             byte[] imageBytes = ImageUtils.loadImagePixelBytesFromTiffStream(tiffStream, -1, -1, -1, -1, -1, -1);
+            assertNotNull(imageBytes);
             // save the bytes as tiff to make sure they can be read
-            int imageSize = imageInfo.sx * imageInfo.sy * (imageInfo.cmPixelSize / 8);
-            Iterator<BufferedImage> pagesIterator = IntStream.range(0, imageInfo.sz)
-                    .mapToObj(sliceIndex -> {
-                        BufferedImage bufferedImage = new BufferedImage(imageInfo.sx, imageInfo.sy, BufferedImage.TYPE_USHORT_GRAY);
-                        int bufferOffset = sliceIndex * imageSize;
-                        for (int y = 0; y < imageInfo.sy; y++) {
-                            for (int x = 0; x < imageInfo.sx; x++) {
-                                int pixel = imageBytes[bufferOffset] & 0x000000ff;
-                                pixel |= (imageBytes[bufferOffset + 1] & 0x000000ff) << 8;
-                                bufferedImage.setRGB(x, y, pixel);
-                                bufferOffset += 2;
-                            }
-                        }
-                        return bufferedImage;
-                    })
-                    .iterator();
-            File testOutputFile = testDirectory.resolve("outputTiff.tif").toFile();
-            OutputStream testOutputStream = Files.newOutputStream(testOutputFile.toPath());
-            TIFFEncodeParam param = new TIFFEncodeParam();
-            ImageEncoder encoder = ImageCodec.createImageEncoder("tiff", testOutputStream, param);
-            param.setExtraImages(pagesIterator);
-            encoder.encode(pagesIterator.next());
-            testOutputStream.flush();
-            testOutputStream.close();
-            try (FileSeekableStream tiffResultStream = new FileSeekableStream(testOutputFile)) {
+            Path testOutputFilePath = testDirectory.resolve("outputTiff.tif");
+            try (OutputStream testOutputStream = Files.newOutputStream(testOutputFilePath)) {
+                TIFFEncodeParam param = new TIFFEncodeParam();
+                ImageEncoder encoder = ImageCodec.createImageEncoder("tiff", testOutputStream, param);
+                Iterator<BufferedImage> pagesIterator = IntStream.range(0, imageInfo.sz)
+                        .mapToObj(sliceIndex -> extractSliceFromImageBytes(imageBytes, imageInfo, sliceIndex))
+                        .iterator();
+                param.setExtraImages(pagesIterator);
+                encoder.encode(pagesIterator.next());
+                testOutputStream.flush();
+            }
+            try (FileSeekableStream tiffResultStream = new FileSeekableStream(testOutputFilePath.toFile())) {
                 RenderedImageInfo resultImageInfo = ImageUtils.loadImageInfoFromTiffStream(tiffResultStream);
+                assertNotNull(resultImageInfo);
                 assertEquals(imageInfo.sx, resultImageInfo.sx);
                 assertEquals(imageInfo.sy, resultImageInfo.sy);
                 assertEquals(imageInfo.sz, resultImageInfo.sz);
@@ -98,19 +85,34 @@ public class ImageUtilsTest {
         }
     }
 
+    private BufferedImage extractSliceFromImageBytes(byte[] imageBytes, RenderedImageInfo imageInfo, int sliceIndex) {
+        BufferedImage bufferedImage = new BufferedImage(imageInfo.sx, imageInfo.sy, BufferedImage.TYPE_USHORT_GRAY);
+        int sliceSize = imageInfo.sx * imageInfo.sy * (imageInfo.cmPixelSize / 8);
+        int bufferOffset = sliceIndex * sliceSize;
+        for (int y = 0; y < imageInfo.sy; y++) {
+            for (int x = 0; x < imageInfo.sx; x++) {
+                int pixel = imageBytes[bufferOffset] & 0x000000ff;
+                pixel |= (imageBytes[bufferOffset + 1] & 0x000000ff) << 8;
+                bufferedImage.setRGB(x, y, pixel);
+                bufferOffset += 2;
+            }
+        }
+        return bufferedImage;
+    }
+
     @Test
     public void combineSlices() {
         TestUtils.prepareTestDataFiles(Paths.get(TEST_DATADIR), testDirectory, "default.0.tif", "default.1.tif");
-        File testFile0 = testDirectory.resolve("default.0.tif").toFile();
-        File testFile1 = testDirectory.resolve("default.1.tif").toFile();
+        Path testFile0 = testDirectory.resolve("default.0.tif");
+        Path testFile1 = testDirectory.resolve("default.1.tif");
 
         byte[] contentBytes = ImageUtils.bandMergedTextureBytesFromImageStreams(
                 Stream.of(testFile0, testFile1)
-                                .map(f  -> NamedSupplier.namedSupplier(
-                                        f.getName(),
+                                .map(p  -> NamedSupplier.namedSupplier(
+                                        p.getFileName().toString(),
                                         () -> {
                                             try {
-                                                return Files.newInputStream(f.toPath());
+                                                return Files.newInputStream(p);
                                             } catch (IOException e) {
                                                 throw new UncheckedIOException(e);
                                             }
